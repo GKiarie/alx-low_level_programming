16071abc8100b00cfe4354295c339697be74d5e5
Isaac Gitau <igitaukiarie@gmail.com>
2022-06-30 12:44:17 -0700

    last qn

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,7 @@
  */
 int main(void)
 {
-	write(fd, "and that piece of art is useful\" - Dora Korpar, 2-15-10-10\n", 58);
-
-	close(fd);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 
 	return (1);
 }
