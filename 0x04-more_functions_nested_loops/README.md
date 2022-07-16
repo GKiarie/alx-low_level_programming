d9421f492eede1f8dfcedadacb7f89aaa2c8bee4
Isaac Gitau <igitaukiarie@gmail.com>
2022-07-16 00:28:25 -0700

    function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+more functions and nested loops
+##0.isupper
+function that checks for uppercase character
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  * Header file
+  * Header - contains all the prototypes for the 
+  * Directory 0x04
+  */
+
+int _isupper(int c);
+int _isupper(int c);
+
+#endif
