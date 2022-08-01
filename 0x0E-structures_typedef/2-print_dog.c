2305c7310fe0bec97b4d0ee81a830cf99ef81eec
Isaac Gitau <igitaukiarie@gmail.com>
2022-08-01 13:44:40 -0700

    structs

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,20 +16,18 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: (nil)\n");
 	}
+	else
+	{
+		printf("Name: %s\n", (*d).name);
+	}
+
 	if ((*d).owner == NULL)
 	{
 		printf("Name: (nil)\n");
 	}
-	/* if ((*d).age == NULL)
+	else
 	{
-		printf("Age: (nil)\n");
-	} */
-
-
-	if (d != 0)
-	{
-		printf("Name: %s\n", (*d).name);
-		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner);
 	}
+	printf("Age: %f\n", (*d).age);
 }
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdlib.h>
+
 
 /**
   * new_dog - function that creates a new dog
@@ -10,4 +12,27 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *ptr;
 
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	if (name == NULL)
+	{
+		free(ptr);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(ptr);
+		free(name);
+		return (NULL);
+	}
+	(*ptr).name = name;
+	(*ptr).age = age;
+	(*ptr).owner = owner;
+	return (ptr);
+}
