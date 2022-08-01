f0aa487b6a82a2e67fce4143e5a5143c1d6d3225
Isaac Gitau <igitaukiarie@gmail.com>
2022-08-01 11:54:05 -0700

    struct definition

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+Structures, Typedef
+
+##0. Poppy
+Type struct dog with the followinmg elements:
+	name, type = char *
+	age, type = float
+	owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - structure, dog description
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner name
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
