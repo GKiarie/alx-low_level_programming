4be9d730f739097ee4f77b3e7c6a2532a1cf33c0
Isaac Gitau <igitaukiarie@gmail.com>
2022-09-26 11:46:26 -0700

    betty styling

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,18 +7,18 @@
  * @s: input string
  * @c: character to be checked
  * Return: pointer to first occurence of char c in string s
- * Return: NULL is char not found
+ * NULL if char not found
  */
 
 char *_strchr(char *s, char c)
 {
-  while (*s != '\0')
-    {
-      if (*s == c)
+	while (*s != '\0')
 	{
-	  return ((char *) s);
+		if (*s == c)
+		{
+			return ((char *) s);
+		}
+		s++;
 	}
-      s++;
-    }
-  return NULL;
+	return (NULL);
 }
