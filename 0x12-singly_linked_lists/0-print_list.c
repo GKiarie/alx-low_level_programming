e0800200dec545289b6774b4afdc11654644e09f
Isaac Gitau <igitaukiarie@gmail.com>
2022-10-24 10:17:05 -0700

    print all elements hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,20 +9,18 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] %p\n", h->str);
-			h = h->next;
-			n++;
+			printf("[0] (nil)\n");
 		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			n++;
 		}
+		n++;
+		h = h->next;
 	}
 	return (n);
 }
