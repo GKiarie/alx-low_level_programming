f9b4cb388ba6743005f5ca967dd015052ced6e3a
Isaac Gitau <igitaukiarie@gmail.com>
2022-10-24 10:57:49 -0700

    function that adds a new node at the beginning of a list_t list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+  * _strlen - ftn that finds len of a string
+  * @str: pointer to string
+  * Return: size of string
+  */
+
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+  * add_node - function that adds a new
+  * node at the beginning of a list_t list
+  * @head: pointer to lionked list ptr
+  * @str: string to be linked
+  * Return: address of new element, NULL
+  * on failure
+  */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node_new;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	node_new = malloc(sizeof(list_t));
+
+	if (node_new == NULL)
+	{
+		return (NULL);
+	}
+
+	node_new->str = strdup(str);
+
+	if (node_new == NULL)
+	{
+		free(node_new);
+		return (NULL);
+	}
+	node_new->len = _strlen(node_new->str);
+	node_new->next = *head;
+	*head = node_new;
+	return (node_new);
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -5,3 +5,6 @@ function that prints all the elements of a list_t list.
 
 ##1. List length
 function that returns the number of elements in a linked list_t list.
+
+##2. Add node
+function that adds a new node at the beginning of a list_t list
