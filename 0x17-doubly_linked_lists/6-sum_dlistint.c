a2758dd250a2d0be2d4003d85a5d3af13a5ce3e1
GKiarie <igitaukiarie@gmail.com>
2022-12-08 10:55:38 -0800

    task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -8,5 +8,16 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-  fg
-    
+  int sum = 0;
+
+  if (head == NULL)
+    {
+      return (0);
+    }
+  while (head)
+    {
+      sum += head->n;
+      head = head->next;
+    }
+  return (sum);
+}
