49d20bc4104eabfc8071c3e84594fdbabcb3d96a
GKiarie <igitaukiarie@gmail.com>
2023-02-23 12:17:54 -0800

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Interview Question
+"""
+
+def island_perimeter(grid):
+    """
+    Perimeter of island
+    """
+    perimeter = 0
+
+    for i, j in enumerate(grid):
+        for x, y in enumerate(j):
+            if y == 1:
+                if i == 0 or grid[i - 1][x] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][x] == 0:
+                    perimeter += 1
+                if x == 0 or grid[i][x - 1] == 0:
+                    perimeter += 1
+                if x == len(j) - 1 or grid[i][x + 1] == 0:
+                    perimeter += 1
+    return perimeter
